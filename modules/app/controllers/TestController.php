bbbef84d6dbc965b7cdfabe77e30c6e69ea4ab9a
liuhao <liu546hao@163.com>
2018-01-18 11:07:46 +0800

    add git ignore

--- modules/app/controllers/TestController.php
+++ modules/app/controllers/TestController.php
@@ -8,27 +8,10 @@
 
 namespace root\modules\app\controllers;
 
-use libphonenumber\NumberParseException;
-use libphonenumber\PhoneNumberFormat;
-use libphonenumber\PhoneNumberUtil;
 class TestController
 {
     public function index()
     {
 
-        $phoneUtil = PhoneNumberUtil::getInstance();
-
-        $numberString = "";
-
-        try {
-            $numberPrototype = $phoneUtil->parse($numberString, "IN");
-
-            echo "Input: " .          $numberString . "\n";
-            echo "isValid: " .       ($phoneUtil->isValidNumber($numberPrototype) ? "true" : "false") . "\n";
-            echo "E164: " .           $phoneUtil->format($numberPrototype, PhoneNumberFormat::E164) . "\n";
-            echo "National: " .       $phoneUtil->format($numberPrototype, PhoneNumberFormat::NATIONAL) . "\n";
-            echo "International: " .  $phoneUtil->format($numberPrototype, PhoneNumberFormat::INTERNATIONAL) . "\n";
-        } catch (NumberParseException $e) {
-        }
     }
 }
